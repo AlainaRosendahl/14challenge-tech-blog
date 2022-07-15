5183276a31a630ee2fc2dc7272a004d524af9d3e
Alaina Rosendahl <elenarosendahl16@gmail.com>
2022-07-15 17:15:03 -0500

    create post link broken

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
     Post.findAll({
@@ -53,7 +54,7 @@ router.get('/', (req, res) => {
       });
   });
   
-  router.post('/', (req, res) => {
+  router.post('/', withAuth, (req, res) => {
     Post.create({
       title: req.body.title,
       post_url: req.body.post_url,
@@ -66,7 +67,7 @@ router.get('/', (req, res) => {
       });
   });
   
-  router.put('/:id', (req, res) => {
+  router.put('/:id', withAuth, (req, res) => {
     Post.update(
       {
         title: req.body.title
@@ -90,7 +91,7 @@ router.get('/', (req, res) => {
       });
   });
   
-  router.delete('/:id', (req, res) => {
+  router.delete('/:id', withAuth, (req, res) => {
     console.log('id', req.params.id);
     Post.destroy({
       where: {
